1f8cb14a4f7bbd3da3e9e21bf2440431f8963f7b
ANSHUL-2021WB86628 <2021WB86628@wilp.bits-pilani.ac.in>
2025-07-14 17:18:07 +0530

    Update App.java
    new

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(MESSAGE);
 	System.out.println("Hello Jenkins CLI");
+	    System.out.println("Hello Jenkins CLI");
     }
 
     public String getMessage() {
